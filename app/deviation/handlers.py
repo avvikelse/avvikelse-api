2cbb45531782170eb6bfed2d403012b14821bc9c
Johan Nilsson <github@markupartist.com>
2011-10-08 15:08:33 +0200

    Sample data.

--- app/deviation/handlers.py
+++ app/deviation/handlers.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-
+import datetime
 from bottle import Bottle, abort
 
 deviation_app = Bottle()
@@ -8,9 +8,18 @@ deviation_app = Bottle()
 def query(version):
     abort(501)
 
-@deviation_app.route('/v:version/:deviation_id')
+@deviation_app.route('/v:version/:deviation_id/')
 def get_deviation(version, deviation_id):
-    abort(501)
+    return {
+        'deviation': {
+            'title': 'Stopp vid TCE',
+            'description': None,
+            'latitude': '18.000',
+            'longitude': '58.000',
+            'line_number': '4',
+            'created_at': str(datetime.datetime.utcnow())
+        }
+    }
 
 @deviation_app.route('/v:version/', method='POST')
 def create_update_deviation(version):
