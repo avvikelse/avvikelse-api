883c01a7579c0c3edfa1e46024b35f65f6ce901b
Johan Nilsson <github@markupartist.com>
2011-10-09 03:00:10 +0200

    Added jsonp support.

--- app/deviation/handlers.py
+++ app/deviation/handlers.py
@@ -1,14 +1,44 @@
 # -*- coding: UTF-8 -*-
 from datetime import datetime, timedelta
 import logging
-from bottle import Bottle, abort, request, response
+from bottle import Bottle, abort, request, response, json_dumps
 from deviation.models import Deviation
 
 logger = logging.getLogger(__name__)
 
 deviation_app = Bottle()
 
+
+def makeJSONP(callback, data):
+    """ wraps the data in callback to prep it for JSONP response
+
+        borrowed/stolen from https://github.com/foxbunny/HTTPy-RPC/blob/master/app.py
+    """
+    response_data = callback
+    response_data += '('
+    response_data += json_dumps(data)
+    response_data += ');'
+    return response_data
+
+def auto_jsonp(f):
+    """ Automatically convert JSON response to JSONP
+
+        borrowed/stolen from https://github.com/foxbunny/HTTPy-RPC/blob/master/app.py
+    """
+    def new(*arg, **kw):
+        callback = request.GET.get('callback')
+        result_data = f(*arg, **kw)
+        if callback and isinstance(result_data, dict):
+            # We only do JSONP for dicts
+            response.headers['Content-type'] = 'text/javascript'
+            return makeJSONP(callback, result_data)
+        # otherwise, we just return as usual
+        return result_data
+
+    return new
+
 @deviation_app.route('/status/')
+@auto_jsonp
 def status():
     line = request.GET.get('line', None)
     vehicle = request.GET.get('vehicle', None)
@@ -48,6 +78,7 @@ def status():
     }
 
 @deviation_app.route('/:deviation_id/')
+@auto_jsonp
 def get_deviation(deviation_id):
     try:
         deviation = Deviation.objects.get(id=deviation_id)
@@ -69,6 +100,7 @@ def get_deviation(deviation_id):
     }
 
 @deviation_app.route('/', method='POST')
+@auto_jsonp
 def create_deviation():
     deviation = Deviation()
     deviation.comment = request.POST.get('comment', None)
